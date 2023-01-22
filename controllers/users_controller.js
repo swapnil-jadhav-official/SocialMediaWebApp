ce56e8921f7fd81aa77c10cb8157e6e294e450eb
Swapnil <swapnil.jadhav.0097@gmail.com>
2023-01-22 16:17:11 +0530

    adding changes for popup  notification using noty

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,14 +1,16 @@
 const User = require('../models/user');
 
-
+// let's keep it same as before
 module.exports.profile = function(req, res){
-    User.findById(req.params.id, function(err,user){
+    User.findById(req.params.id, function(err, user){
         return res.render('user_profile', {
             title: 'User Profile',
-            profile_user : user
+            profile_user: user
         });
     });
+
 }
+
 
 module.exports.update = function(req, res){
     if(req.user.id == req.params.id){
@@ -19,6 +21,7 @@ module.exports.update = function(req, res){
         return res.status(401).send('Unauthorized');
     }
 }
+
 
 // render the sign up page
 module.exports.signUp = function(req, res){
@@ -47,20 +50,24 @@ module.exports.signIn = function(req, res){
 // get the sign up data
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
+        req.flash('error','Password and Confirm Password is not Matching!')
         return res.redirect('back');
     }
 
     User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
+        if(err){ req.flash('error','Error in finding user'); return}
 
         if (!user){
             User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
+                if(err){req.flash('error','Error in Creating user'); return}
 
+                req.flash('success','User Created Successfully!');
                 return res.redirect('/users/sign-in');
             })
         }else{
+            req.flash('error','User already exists');
             return res.redirect('back');
+            
         }
 
     });
@@ -69,11 +76,12 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success','Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
-
+    req.flash('success','You have logged Out..!');
     return res.redirect('/');
 }
